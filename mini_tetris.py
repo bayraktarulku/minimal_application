c05e7d79796dfd057b668a562f79a5b4c8500d09
ulku <ulku.bayraktar@beyaz.net>
2019-10-16 10:06:21 +0300

    shape settings

--- mini_tetris.py
+++ mini_tetris.py
@@ -4,7 +4,9 @@
 from PyQt5.QtWidgets import QMainWindow, QFrame, QDesktopWidget, QApplication
 from PyQt5.QtCore import Qt, QBasicTimer, pyqtSignal
 from PyQt5.QtGui import QPainter, QColor
-import sys, random
+import sys
+import random
+
 
 class Tetris(QMainWindow):
 
@@ -59,7 +61,7 @@ class Board(QFrame):
         self.setFocusPolicy(Qt.StrongFocus)
         self.isStarted = False
         self.isPaused = False
-
+        self.clearBoard()
 
     def start(self):
         if self.isPaused:
@@ -67,9 +69,67 @@ class Board(QFrame):
 
         self.isStarted = True
         self.isWaitingAfterLine = False
+        # self.numLinesRemoved = 0
+        self.clearBoard()
 
         self.msg2Statusbar.emit(str(self.numLinesRemoved))
 
+        # self.newPiece()
+        self.timer.start(Board.Speed, self)
+
+    def clearBoard(self):
+        # for i in range(Board.BoardHeight * Board.BoardWidth):
+        #     self.board.append(Tetrominoe.NoShape)
+        pass
+
+    def newPiece(self):
+        '''creates a new shape'''
+
+        self.curPiece = Shape()
+        self.curPiece.setRandomShape()
+        self.curX = Board.BoardWidth // 2 + 1
+        self.curY = Board.BoardHeight - 1 + self.curPiece.minY()
+
+        if not self.tryMove(self.curPiece, self.curX, self.curY):
+
+            self.curPiece.setShape(Tetrominoe.NoShape)
+            self.timer.stop()
+            self.isStarted = False
+            self.msg2Statusbar.emit("Game over")
+
+
+class Tetrominoe(object):
+
+    NoShape = 0
+    ZShape = 1
+    SShape = 2
+    LineShape = 3
+    TShape = 4
+    SquareShape = 5
+    LShape = 6
+    MirroredLShape = 7
+
+
+class Shape(object):
+
+    coordsTable = (((0, 0),     (0, 0),     (0, 0),     (0, 0)),
+                   ((0, -1),    (0, 0),     (-1, 0),    (-1, 1)),
+                   ((0, -1),    (0, 0),     (1, 0),     (1, 1)),
+                   ((0, -1),    (0, 0),     (0, 1),     (0, 2)),
+                   ((-1, 0),    (0, 0),     (1, 0),     (0, 1)),
+                   ((0, 0),     (1, 0),     (0, 1),     (1, 1)),
+                   ((-1, -1),   (0, -1),    (0, 0),     (0, 1)),
+                   ((1, -1),    (0, -1),    (0, 0),     (0, 1)))
+
+    def __init__(self):
+
+        self.coords = [[0, 0] for i in range(4)]
+        self.pieceShape = Tetrominoe.NoShape
+
+        self.setShape(Tetrominoe.NoShape)
+
+    def shape(self):
+        return self.pieceShape
 
 
 if __name__ == '__main__':
