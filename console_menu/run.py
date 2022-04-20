9ec4cae148104d5e5fa6a151cb0ff032275b3571
ulku bayraktar <ulkubayraktar@gmail.com>
2022-04-20 10:21:04 +0300

    update delete methods completed

--- console_menu/run.py
+++ console_menu/run.py
@@ -30,17 +30,30 @@ def add():
 
 
 def update():
-    print("update")
+    print("############## Update record in list ##############")
+    df = list()
+    idx = int(input("line number you want to update: "))
+    cls()
+    print(df.iloc[[idx]])
+    name = input("Name: ")
+    surname = input("Surname: ")
+    age = input("Age: ")
+    df.iloc[idx] = [name, surname, age]
+    df.to_csv("file.csv", index=False)
+    print("Updated")
 
 
 def delete():
+    print("############## Delete record from list ##############")
     df = list()
-    idx = input("line number you want to delete: ")
+    idx = int(input("line number you want to delete: "))
     cls()
     print(df.iloc[[idx]])
     if click.confirm("Do you want to continue?", default=True):
         print("Do something")
-        # df.drop(1)
+        dfx = df.drop([idx])
+        dfx.to_csv("file.csv", index=False)
+    print("Record Delete")
 
 
 menu = ConsoleMenu(
