5c20ae5108c6b3c573cd90d0daeb5ec585df16a7
ulku bayraktar <ulkubayraktar@gmail.com>
2022-07-08 11:43:35 +0300

    fastapi miniman example (WIP)

---
+++ fastapi_example/main.py
@@ -0,0 +1,54 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.params import Body
+from models import Todo
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+data = [
+    {"id": 1, "text": "First Task", "isCompleted": True},
+    {"id": 2, "text": "This is fastapi example", "isCompleted": False},
+    {"id": 3, "text": "XXXX", "isCompleted": False},
+]
+
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/todos")
+def read_todos():
+
+    return data
+
+
+@app.put("/todos/{todo_id}")
+def update_todo(todo_id: int, todo_obj: Todo):
+    for todo in data:
+        if todo["id"] == todo_id:
+            todo["isCompleted"] = todo_obj.isCompleted
+            return todo
+
+
+@app.post("/todos")
+def create_todo(todo_obj: Todo):
+    todo_obj.id = len(data) + 1
+    data.append(todo_obj.dict())
+    return todo_obj
+
+
+@app.delete("/todos/{todo_id}")
+def delete_todo(todo_id: int):
+    for todo in data:
+        if todo["id"] == todo_id:
+            data.remove(todo)
+            return {"message": "Todo deleted"}
